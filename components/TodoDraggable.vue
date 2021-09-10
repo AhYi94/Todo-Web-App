35a4cd097a4c248b08835651257d7e9fb4b15fe5
Adam <lingyunggie@gmail.com>
2021-09-11 00:15:20 +0800

    todo done

---
+++ components/TodoDraggable.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="w-1/5">
+    <div class="bg-gray-500 p-4 flex justify-between">
+      <h1 class="text-white text-2xl self-center">{{ title }}</h1>
+      <div class="shadow-inner text-center p-2 bg-white">
+        <b>{{ countListItem }}</b> <br />Projects
+      </div>
+    </div>
+    <draggable :list="list" group="task">
+      <div
+        v-for="element in list"
+        :key="element.name"
+        class="bg-white p-5 border-b-2 break-all"
+      >
+        {{ element.name }}
+      </div>
+    </draggable>
+  </div>
+</template>
+<script>
+import draggable from 'vuedraggable'
+export default {
+  components: {
+    draggable,
+  },
+  props: {
+    title: { type: String, default: '' },
+    list: {
+      type: Array,
+      default() {
+        return []
+      },
+    },
+  },
+  computed: {
+    countListItem() {
+      return this.list.length
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,101 @@
 <template>
-  <Tutorial/>
+  <div class="mx-20">
+    <div class="flex justify-between my-5">
+      <div class="my-5 w-1/3">
+        <div class="flex">
+          <input
+            v-model="newItem.name"
+            class="
+              shadow
+              appearance-none
+              border
+              rounded
+              w-full
+              py-2
+              px-3
+              text-gray-700
+              leading-tight
+              focus:outline-none focus:shadow-outline
+            "
+          />
+          <button
+            class="
+              bg-green-500
+              hover:bg-green-700
+              text-white
+              font-bold
+              w-1/4
+              py-1
+              px-1
+              ml-5
+              rounded
+            "
+            @click="addItem"
+          >
+            Add Project
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <div class="ml-auto text-center">
+          <label class="uppercase font-bold"> Total </label>
+          <div class="shadow-inner text-center p-2 bg-white">
+            <b>{{ totalItems }}</b> <br />Projects
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex justify-between">
+      <todo-draggable title="To Do" :list="todo" />
+      <todo-draggable title="In Progress" :list="inProgress" />
+      <todo-draggable title="Done" :list="done" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import TodoDraggable from '~/components/TodoDraggable.vue'
+export default {
+  components: { TodoDraggable },
+  data() {
+    return {
+      todo: [
+        { name: 'Project 1', id: 1 },
+        { name: 'Project 2', id: 2 },
+        { name: 'Project 3', id: 3 },
+        { name: 'Project 4', id: 4 },
+      ],
+      inProgress: [
+        { name: 'Project 5', id: 1 },
+        { name: 'Project 6', id: 2 },
+      ],
+      done: [
+        { name: 'Project 7', id: 1 },
+        { name: 'Project 8', id: 2 },
+        { name: 'Project 9', id: 2 },
+      ],
+      newItem: { name: null, id: null, list: null },
+    }
+  },
+  computed: {
+    totalItems() {
+      const totalProject =
+        this.todo.length + this.inProgress.length + this.done.length
+      return totalProject
+    },
+  },
+  methods: {
+    addItem() {
+      if (!this.newItem.name) {
+        alert("Title shouldn't be empty")
+        return
+      }
+      this.newItem.id = this.todo.length + 1
+      this.newItem.todo = 1
+      this.todo.push({ ...this.newItem })
+      this.newItem.name = null
+    },
+  },
+}
 </script>
